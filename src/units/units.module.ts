220efa5fcb10070f9d0a09d8f2a52e6ffe957689
Tilav <shavqiddintilovov0@gmail.com>
2026-04-10 16:33:27 +0500

    .

--- src/units/units.module.ts
+++ src/units/units.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UnitsController } from './units.controller';
 import { UnitsService } from './units.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [UnitsController],
   providers: [UnitsService],
   exports: [UnitsService],
